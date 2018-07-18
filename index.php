0e13ace4ea9874bad844f36b535ca38df4c00ad4
Watcharamet <watcharamet@m2spop.com>
2018-07-18 18:40:19 +0700

    debug

--- index.php
+++ index.php
@@ -1,20 +1,16 @@
 <?php
 
+// Token
+$channel_token = 'vWFV8hGQvQn/p8BCtcDjRWqmLpBaieSE46aGss4dS6pg99ovTx3aLRw8h1VOQqqJLJInHe9558Vn9XEMgUKvLnl176l4I9LVxsKoJpL/Ys9kAoRlB6rEfYcybyWNbEuU9Y/AV63nQRheUY3lBg9KcQdB04t89/1O/w1cDnyilFU=';
+$channel_secret = 'c6b8cd0c86c93eb91e33ca4a18cd45f4';
+
 // Get message from Line API
 $content = file_get_contents('php://input');
 $events = json_decode($content, true);
 
 if (!is_null($events['events'])) {
 
-    require_once('./LINEBotTiny.php');
-
-    // Token
-    $channel_token = 'vWFV8hGQvQn/p8BCtcDjRWqmLpBaieSE46aGss4dS6pg99ovTx3aLRw8h1VOQqqJLJInHe9558Vn9XEMgUKvLnl176l4I9LVxsKoJpL/Ys9kAoRlB6rEfYcybyWNbEuU9Y/AV63nQRheUY3lBg9KcQdB04t89/1O/w1cDnyilFU=';
-    $channel_secret = 'c6b8cd0c86c93eb91e33ca4a18cd45f4';
-
-    $client = new LINEBotTiny($channel_token, $channel_secret);
-
-    // Loop through each event
+	// Loop through each event
 	foreach ($events['events'] as $event) {
     
         // When user click on rich menu
@@ -25,6 +21,11 @@ if (!is_null($events['events'])) {
 
             switch($event['message']['text']) {
                 case 'FAQ':
+
+                    $headers = array();
+                    $headers[] = "Content-Type: application/json";
+                    $headers[] = "Authorization: Bearer {$channel_secret}";
+
                     $messages = array();
                     #Message 1
                     $messages[] = array(
@@ -46,23 +47,46 @@ if (!is_null($events['events'])) {
                             ),
                         )
                     );
+                    #Separator
+                    $messages[] = array(
+                        'type' => 'separator'
+                    );
+                    #Message 2
+                    $messages[] = array(
+                        'type' => 'box',
+                        'layout' => 'vertical',
+                        'spacing' => 'sm',
+                        'contents' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => 'HOW DO I MAKE PAYMENTS USING PAYPAL? HOW DOES IT WORK?',
+                                'wrap' => true,
+                                'color' => '#000000',
+                            ),
+                            array(
+                                'type' => 'text',
+                                'text' => 'Paypal is the easiest way to make payments online. While checking out your order, you will be redirected to the Paypal website. Be sure to fill in correct details for fast & hassle-free payment processing. After a successful Paypal payment, a payment advice will be automatically generated to Samplestore.com system for your order.',
+                                'wrap' => true,
+                                'color' => '#000000',
+                            ),
+                        )
+                    );
 
-
-                    error_log(json_encode($messages));
-
-                    // Send message to Customer through LINE
-                    $client->replyMessage(array(
+                    // Create message for send to LINE Server
+                    $message = [
                         'replyToken' => $replyToken,
                         'messages' => array(
-                            'type' => 'text',
-                            'messages' => array(
-                                array(
-                                    'type' => 'text',
-                                    'text' => $message['text']
-                                )
-                            )
-                        )
-                    ));
+                            'type' => 'bubble',
+                            'body' => array(
+                                'type' => 'box',
+                                'layout' => 'vertical',
+                                'spacing' => 'md',
+                                'contents' => $messages,
+                            ),
+                        ),
+                    ];
+
+                    send_message_to_line($access_token, $message);
 
                     break;
             }
@@ -70,5 +94,27 @@ if (!is_null($events['events'])) {
 	}
 }
 
+/**
+ * Call curl to send message to LINE
+ *
+ * @param string $access_token
+ * @param array $message
+ */
+function send_message_to_line($access_token, $message){
+    $url = 'https://api.line.me/v2/bot/message/reply';
+
+    $headers = array();
+    $headers[] = "Content-Type: application/json";
+    $headers[] = "Authorization: Bearer {$access_token}";
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($message));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+}
 
 echo "OK";
