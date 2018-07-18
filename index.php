e289b19e6bd10123f8a6a33242cdbb3b38e6ffdb
Watcharamet <watcharamet@m2spop.com>
2018-07-18 18:01:31 +0700

    ส่งเมสเสจ Q & A กลับไปให้ customer เมื่อเขาคลิกปุ่ม Q & A

--- index.php
+++ index.php
@@ -1,35 +1,4 @@
 <?php
-/*
- {
-  "events": [
-      {
-        "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-        "type": "message",
-        "timestamp": 1462629479859,
-        "source": {
-             "type": "user",
-             "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
-         },
-         "message": {
-             "id": "325708",
-             "type": "text",
-             "text": "Hello, world"
-          }
-      }
-  ]
-}
- */
-
-require_once('./vendor/autoload.php');
-
-// Namespace
-use \LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use \LINE\LINEBot;
-use \LINE\LINEBot\MessageBuilder\TextMessageBuilder;
-
-// Token
-$channel_token = 'vWFV8hGQvQn/p8BCtcDjRWqmLpBaieSE46aGss4dS6pg99ovTx3aLRw8h1VOQqqJLJInHe9558Vn9XEMgUKvLnl176l4I9LVxsKoJpL/Ys9kAoRlB6rEfYcybyWNbEuU9Y/AV63nQRheUY3lBg9KcQdB04t89/1O/w1cDnyilFU=';
-$channel_secret = 'c6b8cd0c86c93eb91e33ca4a18cd45f4';
 
 // Get message from Line API
 $content = file_get_contents('php://input');
@@ -37,7 +6,15 @@ $events = json_decode($content, true);
 
 if (!is_null($events['events'])) {
 
-	// Loop through each event
+    require_once('./LINEBotTiny.php');
+
+    // Token
+    $channel_token = 'vWFV8hGQvQn/p8BCtcDjRWqmLpBaieSE46aGss4dS6pg99ovTx3aLRw8h1VOQqqJLJInHe9558Vn9XEMgUKvLnl176l4I9LVxsKoJpL/Ys9kAoRlB6rEfYcybyWNbEuU9Y/AV63nQRheUY3lBg9KcQdB04t89/1O/w1cDnyilFU=';
+    $channel_secret = 'c6b8cd0c86c93eb91e33ca4a18cd45f4';
+
+    $client = new LINEBotTiny($channel_token, $channel_secret);
+
+    // Loop through each event
 	foreach ($events['events'] as $event) {
     
         // When user click on rich menu
@@ -47,35 +24,72 @@ if (!is_null($events['events'])) {
             $replyToken = $event['replyToken'];
 
             switch($event['message']['text']) {
-                
-                case 'Open shop':
-                    $columns = array();
+                case 'FAQ':
+                    $messages = array();
+                    #Message 1
+                    $messages[] = array(
+                        'type' => 'box',
+                        'layout' => 'vertical',
+                        'spacing' => 'sm',
+                        'contents' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => 'I AM HAVING PROBLEMS ACCESSING SAMPLE STORE. SOME OF THE PAGES LOOK WEIRD. AM I USING THE RIGHT BROWSER?',
+                                'wrap' => true,
+                                'color' => '#000000',
+                            ),
+                            array(
+                                'type' => 'text',
+                                'text' => 'As Sample Store uses some of the latest graphics designs which may not be supported in lower version of browsers, it is recommended that you use the following browsers to access Sample Store: \n\r 1. Microsoft Internet Explorer Version 10 onwards.',
+                                'wrap' => true,
+                                'color' => '#000000',
+                            ),
+                        )
+                    );
+                    #Separator
+                    $messages[] = array(
+                        'type' => 'separator'
+                    );
+                    #Message 2
+                    $messages[] = array(
+                        'type' => 'box',
+                        'layout' => 'vertical',
+                        'spacing' => 'sm',
+                        'contents' => array(
+                            array(
+                                'type' => 'text',
+                                'text' => 'HOW DO I MAKE PAYMENTS USING PAYPAL? HOW DOES IT WORK?',
+                                'wrap' => true,
+                                'color' => '#000000',
+                            ),
+                            array(
+                                'type' => 'text',
+                                'text' => 'Paypal is the easiest way to make payments online. While checking out your order, you will be redirected to the Paypal website. Be sure to fill in correct details for fast & hassle-free payment processing. After a successful Paypal payment, a payment advice will be automatically generated to Samplestore.com system for your order.',
+                                'wrap' => true,
+                                'color' => '#000000',
+                            ),
+                        )
+                    );
 
-                    $img_url = "https://gloimg.rglcdn.com/rosegal/pdm-product-pic/Clothing/2017/12/28/source-img/20171228165936_78536.jpg";
-
-                    for($i=0;$i<2;$i++) {
-                        $actions = array(
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("สั่งซื้อเสื้อเบอร์ S","https://m2spop.herokuapp.com/buy.php?id=10&size=S"),
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("สั่งซื้อเสื้อเบอร์ M","https://m2spop.herokuapp.com/buy.php?id=10&size=M"),
-                            new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder("สั่งซื้อเสื้อเบอร์ L","https://m2spop.herokuapp.com/buy.php?id=10&size=L"),
-                        );
-
-                        $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Hoodie", "Pellentesque habitant morbi tristique senectus et netus et m", $img_url , $actions);
-                        $columns[] = $column;
-                    }
-
-                    $carousel = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder($columns);
-                    $templateMessageBuilder = new \LINE\LINEBot\MessageBuilder\TemplateMessageBuilder("Carousel Demo", $carousel);
-
-                    
-                    $httpClient = new CurlHTTPClient($channel_token);
-                    $bot = new LINEBot($httpClient, array('channelSecret' => $channel_secret));
-                    $response = $bot->replyMessage($replyToken, $templateMessageBuilder);
+                    // Send message to Customer through LINE
+                    $client->replyMessage(array(
+                        'replyToken' => $replyToken,
+                        'messages' => array(
+                            'type' => 'bubble',
+                            'body' => array(
+                                'type' => 'box',
+                                'layout' => 'vertical',
+                                'spacing' => 'md',
+                                'contents' => $messages,
+                            )
+                        )
+                    ));
 
                     break;
             }
 		}
 	}
 }
+
 
 echo "OK";
