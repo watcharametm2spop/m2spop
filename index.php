2560d42116507b4a025686584e7a1e1689bd820b
Watcharamet <watcharamet@m2spop.com>
2018-07-19 16:23:39 +0700

    Fixed: พาธไฟล์ผิด

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 require __DIR__ . '/vendor/autoload.php';
-require __DIR__ . 'LINEBotTiny.php';
+require __DIR__ . '/LINEBotTiny.php';
 
 use Automattic\WooCommerce\Client as WooCommerceClient;
 
