1a4db1ed17218496efaeb4025dc09c005ea32bb0
Watcharamet <watcharamet@m2spop.com>
2018-07-19 18:22:13 +0700

    debug

--- index.php
+++ index.php
@@ -128,7 +128,7 @@ if (!is_null($events['events'])) {
                             new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('View detail', $product->permalink),
                             new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Order item',$wooEnpoint.'/shop/?add-to-cart='. $product->id),
                         );
-                        $description = 'Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.';
+                        $description = 'Stet clita kasd gubergren';
 
                         $column = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder($product->name, $description, $img_url , $actions);
                         $columns[] = $column;
